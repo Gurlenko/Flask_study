9f3794bb41f86cd5bff80f1fddaab36751191955
Gurlenko <136372389+Gurlenko@users.noreply.github.com>
2024-04-21 23:27:21 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -2,39 +2,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var cartButton = document.getElementById('cart-button');
     var cartMenu = document.getElementById('cart-menu');
 
-    // Обробник події для відкриття / закриття меню корзини
     cartButton.addEventListener('click', function(event) {
-        event.stopPropagation(); // Зупиняємо подальше поширення події, щоб вона не спрацьовувала на document
-        cartMenu.classList.toggle('active'); // Перемикаємо клас 'active' для відображення / приховання меню
+        event.stopPropagation(); 
+        cartMenu.classList.toggle('active'); 
     });
 
-    // Обробник події для закриття меню при кліку за його межами або на хрестик
     document.addEventListener('click', function(event) {
         var isClickInsideCartMenu = cartMenu.contains(event.target);
         var isClickInsideCartButton = cartButton.contains(event.target);
         if (!isClickInsideCartMenu && !isClickInsideCartButton) {
-            cartMenu.classList.remove('active'); // Прибираємо клас 'active', якщо клік був поза меню та кнопкою корзини
+            cartMenu.classList.remove('active'); 
         }
     });
 
-    // Додаємо обробник події для кожного хрестика в корзині
+
     var xButtons = document.querySelectorAll('.x-button');
     xButtons.forEach(function(button) {
         button.addEventListener('click', function(event) {
-            event.preventDefault(); // Забороняємо відправку форми
+            event.preventDefault(); 
             var itemId = button.dataset.itemId;
-            removeFromCart(itemId); // Викликаємо функцію видалення елемента з корзини
-            event.stopPropagation(); // Зупиняємо подальше поширення події, щоб вона не спрацьовувала на решту обробників
+            removeFromCart(itemId); 
+            event.stopPropagation(); 
         });
     });
 });
 
-// Функція для видалення елемента з корзини
+
 function removeFromCart(itemId) {
     fetch('/remove_from_cart', {
         method: 'POST',
         body: new URLSearchParams({
-            'item_id': itemId // Відправляємо ідентифікатор елемента для видалення на сервер
+            'item_id': itemId 
         }),
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
